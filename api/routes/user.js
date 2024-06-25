b973257bbfab14baa7341a138577e5429d7cfe7b
Dourbinaval <98718421+Dourbinaval@users.noreply.github.com>
2024-06-25 17:18:58 +0530

    check2

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const checkauth = require("../middleware/checkauth");
 const user = require("../models/user");
 
